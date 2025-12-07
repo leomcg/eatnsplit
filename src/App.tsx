589e00217ef3023112ac593ad8b7789d3ca3e1df
leomcg <leomcg@gmail.com>
2025-12-07 15:40:50 -0300

    creating friends list

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Linter } from "eslint";
+
 const initialFriends = [
   {
     id: 118836,
@@ -21,8 +23,42 @@ const initialFriends = [
 
 export default function App() {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />;
+      </div>
     </div>
+  );
+}
+
+function FriendsList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      <button className="button">Select</button>
+    </li>
   );
 }
